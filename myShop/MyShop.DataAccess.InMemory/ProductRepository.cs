9689df7bf1ae0a4512a722858319175bdcd8525a
rgrewal <rgrewal@arrow.ca>
2020-06-10 10:54:48 -0700

    added in models for the product and code to store them in memory

---
+++ myShop/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using MyShop.Core;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products;
+
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+            if (products is null) products = new List<Product>();
+        }
+
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+
+        public void Update (Product p)
+        {
+            Product productToUpdate = products.Find(x => x.ID == p.ID);
+            if (productToUpdate is null) throw new Exception("Product not found");
+
+            productToUpdate = p;
+        }
+
+        public Product Find(string ID)
+        {
+            Product product = products.Find(x => x.ID == ID);
+            if (product is null) throw new Exception("Product not found");
+
+            return product;
+        }
+
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+
+        public void Delete(string ID)
+        {
+            Product productToDelete = products.Find(x => x.ID == ID);
+            if (productToDelete is null) throw new Exception("Product not found");
+
+            products.Remove(productToDelete);
+        }
+    }
+}
