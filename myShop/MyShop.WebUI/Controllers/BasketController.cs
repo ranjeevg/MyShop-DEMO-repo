c3f5e131f45f66fc9d7bc1637e99b1584b96f9f4
rgrewal <rgrewal@arrow.ca>
2020-06-12 11:23:43 -0700

    added in user authorization

--- myShop/MyShop.WebUI/Controllers/BasketController.cs
+++ myShop/MyShop.WebUI/Controllers/BasketController.cs
@@ -12,11 +12,14 @@ namespace MyShop.WebUI.Controllers
     {
         IBasketService BasketService;
         IOrderService OrderService;
+        IRepository<Customer> Customers;
 
-        public BasketController(IBasketService basketService, IOrderService orderService) 
+        public BasketController(IBasketService basketService, IOrderService orderService,
+                                IRepository<Customer> customers) 
         { 
             this.BasketService = basketService;
             this.OrderService = orderService;
+            this.Customers = customers;
         }
 
         // GET: Basket
@@ -45,16 +48,36 @@ namespace MyShop.WebUI.Controllers
             return PartialView(basketSummary);
         }
 
+        [Authorize]
         public ActionResult Checkout()
         {
-            return View();
+            Customer customer = Customers.Collection().FirstOrDefault
+                                    (c => c.Email == User.Identity.Name);
+
+            if (!(customer is null))
+            {
+                Order order = new Order()
+                {
+                    FirstName = customer.FirstName,
+                    Surname = customer.LastName,
+                    Email = customer.Email,
+                    City = customer.City,
+                    Street = customer.Street,
+                    Province = customer.Province,
+                    PostalCode = customer.PostalCode
+                };
+                return View(order);
+            }
+            return RedirectToAction("Error");
         }
 
         [HttpPost]
+        [Authorize]
         public ActionResult Checkout (Order order)
         {
             var basketItems = BasketService.GetBasketItems(this.HttpContext);
             order.OrderStatus = "Order Created";
+            order.Email = User.Identity.Name;
 
             // payment processing happens
 
