721ef6af986d87042e63926194fc57c8eb6bb625
rgrewal <rgrewal@arrow.ca>
2020-06-12 08:28:54 -0700

    created a basket item

--- myShop/MyShop.WebUI/App_Start/UnityConfig.cs
+++ myShop/MyShop.WebUI/App_Start/UnityConfig.cs
@@ -2,6 +2,7 @@ using MyShop.Core.Contracts;
 using MyShop.Core.Models;
 using MyShop.DataAccess.InMemory;
 using MyShop.DataAccess.SQL;
+using MyShop.Services;
 using System;
 
 using Unity;
@@ -57,6 +58,7 @@ namespace MyShop.WebUI
             container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
             container.RegisterType<IRepository<Basket>, SQLRepository<Basket>>();
             container.RegisterType<IRepository<BasketItem>, SQLRepository<BasketItem>>();
+            container.RegisterType<IBasketService, BasketService>();
         }
     }
 }
---
+++ myShop/MyShop.WebUI/Controllers/BasketController.cs
@@ -0,0 +1,43 @@
+﻿using MyShop.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        IBasketService BasketService;
+
+        public BasketController(IBasketService basketService) 
+            { this.BasketService = basketService; }
+
+        // GET: Basket
+        public ActionResult Index()
+        {
+            var model = BasketService.GetBasketItems(this.HttpContext);
+            return View(model);
+        }
+
+        public ActionResult AddToBasket(string Id)
+        {
+            BasketService.AddToBasket(this.HttpContext, Id);
+            return View();
+        }
+
+        public ActionResult RemoveFromBasket(string Id)
+        {
+            BasketService.RemoveFromBasket(this.HttpContext, Id);
+            return View();
+        }
+
+        public PartialViewResult BasketSummary()
+        {
+            var basketSummary = BasketService.GetBasketSummary(this.HttpContext);
+
+            return PartialView(basketSummary);
+        }
+    }
+}
